2b88ba8b2554417de6b6179b0aa1e5d5aa822690
John <johnitolee@gmail.com>
2017-08-08 15:39:49 -0700

    add join

--- sqlzoo/lib/07_joins_exercises.rb
+++ sqlzoo/lib/07_joins_exercises.rb
@@ -40,7 +40,11 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
-
+  SELECT title
+  FROM movies
+  JOIN castings ON castings.movie_id = movies.id
+  JOIN actors ON actors.id = castings.actor_id
+  WHERE name = 'Harrison Ford';
   SQL
 end
 
@@ -49,14 +53,22 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
-
+  SELECT title
+  FROM movies
+  JOIN castings ON castings.movie_id = movies.id
+  JOIN actors ON actors.id = castings.actor_id
+  WHERE ord != 1 AND name = 'Harrison Ford';
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
-
+  SELECT title, name
+  FROM movies
+  JOIN castings ON castings.movie_id = movies.id
+  JOIN actors ON actors.id = castings.actor_id
+  WHERE ord = 1 AND yr = 1962;
   SQL
 end
 
@@ -64,7 +76,13 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
-
+  SELECT yr, COUNT(title)
+  FROM movies
+  JOIN castings ON castings.movie_id = movies.id
+  JOIN actors ON actors.id = castings.actor_id
+  WHERE name = 'John Travolta'
+  GROUP BY yr
+  HAVING COUNT(title) >= 2;
   SQL
 end
 
@@ -72,7 +90,15 @@ def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
   execute(<<-SQL)
-
+  SELECT title, name
+  FROM movies
+  JOIN castings ON castings.movie_id = movies.id
+  JOIN actors ON actors.id = castings.actor_id
+  WHERE title IN (
+      SELECT title FROM movies
+      JOIN castings ON castings.movie_id = movies.id
+      JOIN actors ON actors.id = castings.actor_id
+      WHERE name = 'Julie Andrews') AND ord =1;
   SQL
 end
 
@@ -80,7 +106,14 @@ def prolific_actors
   # Obtain a list in alphabetical order of actors who've had at least 15
   # starring roles.
   execute(<<-SQL)
-
+  SELECT name
+  FROM movies
+  JOIN castings ON castings.movie_id = movies.id
+  JOIN actors ON actors.id = castings.actor_id
+  WHERE ord = 1
+  GROUP BY name
+  HAVING COUNT(name) >= 15
+  ORDER BY name;
   SQL
 end
 
@@ -88,13 +121,28 @@ def films_by_cast_size
   # List the films released in the year 1978 ordered by the number of actors
   # in the cast (descending), then by title (ascending).
   execute(<<-SQL)
-
+  SELECT title, COUNT(name)
+  FROM movies
+  JOIN castings ON castings.movie_id = movies.id
+  JOIN actors ON actors.id = castings.actor_id
+  WHERE yr = 1978
+  GROUP BY title
+  ORDER BY COUNT(name) DESC , title ASC;
   SQL
 end
 
 def colleagues_of_garfunkel
   # List all the people who have played alongside 'Art Garfunkel'.
   execute(<<-SQL)
-
+  SELECT name
+  FROM movies
+  JOIN castings ON castings.movie_id = movies.id
+  JOIN actors ON actors.id = castings.actor_id
+  WHERE title IN (
+    SELECT title
+    FROM movies
+    JOIN castings ON castings.movie_id = movies.id
+    JOIN actors ON actors.id = castings.actor_id
+    WHERE name = 'Art Garfunkel') AND name != 'Art Garfunkel';
   SQL
 end
--- sqlzoo/lib/07a_joins_bonus.rb
+++ sqlzoo/lib/07a_joins_bonus.rb
@@ -26,21 +26,30 @@ require_relative './sqlzoo.rb'
 def alison_artist
   # Select the name of the artist who recorded the song 'Alison'.
   execute(<<-SQL)
-
+  SELECT artist
+  FROM albums
+  JOIN tracks ON tracks.album = albums.asin
+  WHERE song = 'Alison';
   SQL
 end
 
 def exodus_artist
   # Select the name of the artist who recorded the song 'Exodus'.
   execute(<<-SQL)
-
+  SELECT artist
+  FROM albums
+  JOIN tracks ON tracks.album = albums.asin
+  WHERE song = 'Exodus';
   SQL
 end
 
 def blur_songs
   # Select the `song` for each `track` on the album `Blur`.
   execute(<<-SQL)
-
+  SELECT song
+  FROM albums
+  JOIN tracks ON tracks.album = albums.asin
+  WHERE albums.title = 'Blur';
   SQL
 end
 
@@ -49,6 +58,12 @@ def heart_tracks
   # the word 'Heart' (albums with no such tracks need not be shown). Order first by
   # the number of such tracks, then by album title.
   execute(<<-SQL)
+  SELECT title, COUNT(tracks.album)
+  FROM albums
+  JOIN tracks ON tracks.album = albums.asin
+  WHERE song LIKE '%Heart%'
+  GROUP BY title
+  ORDER BY COUNT(tracks.album) DESC, title
 
   SQL
 end
@@ -57,6 +72,10 @@ def title_tracks
   # A 'title track' has a `song` that is the same as its album's `title`. Select
   # the names of all the title tracks.
   execute(<<-SQL)
+  SELECT title
+  FROM albums
+  JOIN tracks ON tracks.album = albums.asin
+  WHERE title = song;
   SQL
 end
 
@@ -64,7 +83,11 @@ def eponymous_albums
   # An 'eponymous album' has a `title` that is the same as its recording
   # artist's name. Select the titles of all the eponymous albums.
   execute(<<-SQL)
-
+  SELECT title
+  FROM albums
+  JOIN tracks ON tracks.album = albums.asin
+  WHERE title = artist
+  GROUP BY title;
   SQL
 end
 
@@ -72,7 +95,11 @@ def song_title_counts
   # Select the song names that appear on more than two albums. Also select the
   # COUNT of times they show up.
   execute(<<-SQL)
-
+  SELECT song, COUNT(DISTINCT(title))
+  FROM albums
+  JOIN tracks ON tracks.album = albums.asin
+  GROUP BY song
+  HAVING COUNT(DISTINCT(title)) > 2;
   SQL
 end
 
@@ -81,7 +108,11 @@ def best_value
   # pence. Find the good value albums - show the title, the price and the number
   # of tracks.
   execute(<<-SQL)
-
+  SELECT title, price, COUNT(tracks.album)
+  FROM albums
+  JOIN tracks ON tracks.album = albums.asin
+  GROUP BY title, price
+  HAVING(price/COUNT(tracks.album)) < 0.5;
   SQL
 end
 
@@ -90,7 +121,12 @@ def top_track_counts
   # tracks. List the top 10 albums. Select both the album title and the track
   # count, and order by both track count and title (descending).
   execute(<<-SQL)
-
+  SELECT title, COUNT(tracks)
+  FROM albums
+  JOIN tracks ON tracks.album = albums.asin
+  GROUP BY title
+  ORDER BY COUNT(tracks) DESC
+  LIMIT 10;
   SQL
 end
 
@@ -98,7 +134,13 @@ def rock_superstars
   # Select the artist who has recorded the most rock albums, as well as the
   # number of albums. HINT: use LIKE '%Rock%' in your query.
   execute(<<-SQL)
-
+  SELECT artist, COUNT(DISTINCT (title))
+  FROM albums
+  JOIN styles ON styles.album = albums.asin
+  WHERE style LIKE '%Rock%'
+  GROUP BY artist
+  ORDER BY COUNT(DISTINCT(title)) DESC
+  LIMIT 1;
   SQL
 end
 
